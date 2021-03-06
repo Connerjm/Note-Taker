cbb5b2171a2f6a1e2e950a040fbb6cb05369f0a7
Conner Martin <connerjm2@gmail.com>
2021-03-05 19:11:53 -0800

    Added routes to the server code.

--- server.js
+++ server.js
@@ -14,6 +14,36 @@ app.use(express.json());
 
 /* Routes */
 
+//Get home.
+app.get('/home', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//Get notes.
+app.get('/notes', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//Redirect to home.
+app.get('*', (req, res) =>
+{
+    res.redirect('/home');
+});
+
+//Get api notes
+app.get('/api/notes', (req, res) =>
+{
+    //TODO
+});
+
+//Post api notes
+app.post('/api/notes', (req, res) =>
+{
+    //TODO
+});
+
 /* Server Start */
 
 app.listen(PORT, () => { console.log(`Server is listening on port ${PORT}`); });
